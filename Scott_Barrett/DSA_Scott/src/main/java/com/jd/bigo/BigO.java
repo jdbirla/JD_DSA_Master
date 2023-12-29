e21f62347a2897b1405399a8664518f68fded0a5
JDBIRLA <jitendra.birla21@gmail.com>
2023-12-29 11:55:31 +0530

    linked list

---
+++ Scott_Barrett/DSA_Scott/src/main/java/com/jd/bigo/BigO.java
@@ -0,0 +1,14 @@
+package com.jd.bigo;
+
+public class BigO {
+    public static void main(String[] args) {
+        printItem(10);
+    }
+
+    public static void printItem(int n)
+    {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
+}
